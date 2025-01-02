8058af48b65d318d6a405ad2caed4fdec73b5057
SealBarkler <50695111+SealBarkler@users.noreply.github.com>
2025-01-02 08:55:23 +0100

    added first assignment for number operators

---
+++ assignment_numbers.py
@@ -0,0 +1,75 @@
+# %%
+# write a function that returns the sum of a and b
+def get_sum(a, b):
+    '''
+    returns the sum of a and b
+    '''
+
+    # YOUR CODE HERE
+    
+    return result
+
+# %%
+# write a function that returns the difference of a and b
+def get_difference(a, b):
+    '''
+    returns the difference of a and b
+    '''
+
+    # YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the product of a and b
+def get_product(a, b):
+    '''
+    returns the product of a and b
+    '''
+
+    # YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the quotient of a and b, so a divided by b
+def get_float_quotient(a, b):
+    '''
+    returns the float quotient of a divided by b
+    '''
+
+    # YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the floored quotient of a and b, so the floored value of a divided by b
+def get_floored_quotient(a, b):
+    '''
+    returns the floored quotient of a divided by b
+    '''
+
+    # YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the remainder of a divided by b
+def get_remainder(a, b):
+    '''
+    returns the remainder of a divided by b
+    '''
+    # YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the value of a to the power of b
+def get_power(a, b):
+    '''
+    returns value for a to the power of b
+    '''
+
+    # YOUR CODE HERE
+
+    return result
