ff731baf3d5fa72f865e6a0e48623f946352a035
Lars Kälber <ac144316@uni-stuttgart.de>
2025-01-03 15:28:20 +0100

    some more tests and renaming

---
+++ assignment_0.py
@@ -0,0 +1,230 @@
+####### Assignment 0 #######
+####### Numbers #######
+# %%
+# write a function that returns the sum of a and b
+def get_sum(a, b):
+    '''
+    returns the sum of a and b
+    '''
+
+    # TODO: YOUR CODE HERE
+    
+    return result
+
+# %%
+# write a function that returns the difference of a and b
+def get_difference(a, b):
+    '''
+    returns the difference of a and b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the product of a and b
+def get_product(a, b):
+    '''
+    returns the product of a and b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the quotient of a and b, so a divided by b
+def get_float_quotient(a, b):
+    '''
+    returns the float quotient of a divided by b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the floored quotient of a and b, so the floored value of a divided by b
+def get_floored_quotient(a, b):
+    '''
+    returns the floored quotient of a divided by b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the remainder of a divided by b
+def get_remainder(a, b):
+    '''
+    returns the remainder of a divided by b
+    '''
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the value of a to the power of b
+def get_power(a, b):
+    '''
+    returns value for a to the power of b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+
+####### strings #######
+# %%
+# write a function that adds a variable folder name to the path string 'C:\users\newbie\Documents'
+def get_path(folder_name):
+    '''
+    returns raw string
+    '''
+    def get_concatenated_string(string1, string2):
+        
+        # TODO: YOUR CODE HERE
+
+        return result
+
+    # TODO: YOUR CODE HERE
+
+    result = get_concatenated_string(path, folder_name)
+
+    return result
+
+# %%
+# write a function that removes all blanks, line breaks,... around a word in a string
+def get_rid_of_whitespace(string):
+    '''
+    returns a clean string
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+
+# %%
+# write a function that removes all blanks, line breaks,... leading a word in a string
+def get_rid_of_leading_whitespace(string):
+    '''
+    returns a clean leading of a string
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that removes all blanks, line breaks,... trailing a word in a string
+def get_rid_of_trailing_whitespace(string):
+    '''
+    returns a clean trailing of a string
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns a string consisting of every n-th letter of a string starting from the initial letter
+def get_nth_letter_second(string, n, initial):
+    '''
+    returns every nth letter of a string starting from initial one
+    '''
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the last n letters from a string
+def get_last_n_letters(string, n):
+    '''
+    returns last n letters of a string
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns a string with the first letter changed by alterna
+def get_first_letter_changed(string, alterna):
+    '''
+    returns a string with first letter changed by alterna
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the length of a string
+def get_length_of_string(string):
+    '''
+    returns length of a string
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns a string as a list
+def get_string_as_list(string):
+    '''
+    returns string as list
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that splits an input string at a variable symbol and returns the first part of the string
+def get_first_part_after_split(string, symbol):
+    '''
+    returns the first part of string after a split 
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that splits an input string at a variable symbol and returns the last two parts concatenated
+def get_last_two_parts_concatenated_after_split(string, symbol):
+    '''
+    returns last two parts concatenated after split
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that appends an element to a list
+def get_list_with_appended_element(list, element):
+    '''
+    returns a list to whom an element is appended
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the length of a list
+def get_length_of_list(list):
+    '''
+    returns the length of a list
+    '''
+    
+    # TODO: YOUR CODE HERE
+
+    return result
+
